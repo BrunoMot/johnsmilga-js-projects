ebe42bbe41fa4475976bacd2bc6ac33c64ad6a18
Bruno Mariz <bmarizot@gmail.com>
2021-10-10 12:45:48 -0300

    finished project

--- 1 - Color Flipper/setup/app.js
+++ 1 - Color Flipper/setup/app.js
@@ -1,13 +1,20 @@
-const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 function newColor() {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  let randomColor = '#';
+  for (let index = 0; index < 6; index++) {
+    const element = hex[Math.floor(Math.random() * hex.length)];
+    randomColor += element;
+  }
   return randomColor;
 }
 
 btn.addEventListener('click', () => {
-  color.textContent = `#${newColor()}`;
+  const randomColor = newColor();
+  document.body.style.backgroundColor = randomColor;
+  color.innerText = randomColor;
+  color.style.color = randomColor;
 });
